058d4b829fecc579e04a4f98deffa531fc5270af
Daniil <77277774+shamemask@users.noreply.github.com>
2023-11-10 23:07:15 +0300

    настроил админку на fizuser и uruser

--- admin.py
+++ admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .UserModel import FizUser, UrUser
 
 # Register your models2 here.
-admin.site.register(FizUser)
-admin.site.register(UrUser)
+
+from authentication.UserModel import FizUser, UrUser
+class CustomFizUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'shop_name', 'full_name', 'phone', 'promo_code', 'terms_of_service', 'is_active')
+    list_filter = ('is_active', 'promo_code', 'terms_of_service')
+    search_fields = ('email', 'shop_name', 'full_name', 'phone')
+class CustomUrUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'form', 'shop_name', 'phone', 'full_name', 'city', 'organization_name', 'legal_address', 'inn', 'kpp', 'bank', 'bik', 'account_number', 'correspondent_account', 'terms_of_service', 'is_active')
+    search_fields = ('email', 'shop_name', 'full_name', 'phone', 'organization_name')
+    search_fields = ('is_active', 'terms_of_service')
+
+try:
+    # admin.site.unregister(FizUser)
+    admin.site.register(FizUser, CustomFizUserAdmin)
+except admin.sites.AlreadyRegistered:
+    pass
+try:
+    # admin.site.unregister(UrUser)
+    admin.site.register(UrUser, CustomUrUserAdmin)
+except admin.sites.AlreadyRegistered:
+    pass
